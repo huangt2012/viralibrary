cacbebba207de3c5a41e671ac31da2e15fa1e84e
黄婷 <wb.huangting@mesg.corp.netease.com>
2020-04-20 22:06:40 +0800

    add storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Button from './Button'
+
+const defaultButton = () => (
+    <Button onClick={action('clicked')}>default button</Button>
+)
+
+const buttonWithSize = () => (
+    <>
+        <Button size='lg'>large button</Button>
+        <Button size='sm'>small</Button>
+    </>
+)
+
+const buttonWidthType = () => (
+    <>
+        <Button btnType='primary'>primary</Button>
+        <Button btnType='danger'>danger</Button>
+        <Button btnType='link' href='https://www.baidu.com/'>link</Button>
+    </>
+)
+
+storiesOf('Button', module)
+    .add('默认button', defaultButton)
+    .add('不同尺寸button', buttonWithSize)
+    .add('不同类型button', buttonWidthType);
